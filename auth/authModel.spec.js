7388a4939138ea59791637ffe591e3e1b98a1432
spettigrew <sara.pettigrew5@gmail.com>
2020-01-24 20:26:55 -0700

    Auth model setup

--- auth/authModel.spec.js
+++ auth/authModel.spec.js
@@ -1,4 +1,4 @@
-const db = require("../data/db.config")
+const db = require("../database/dbConfig")
 const authModel = require("./auth-model")
 
 beforeEach(async () => {
@@ -8,13 +8,32 @@ beforeEach(async () => {
 describe("auth model", () => {
     test ("list", async () => {
         const res = await authModel.list()
-        expect(res).toBeGreaterThan(0)
+        expect(res).toBeGreaterThan[1]
     })
 })
 
-// describe("auth model", () => {
-//     test("", async () => {
-//         const res = await
-//             expect().
-//     })
-// })
+describe("auth model", () => {
+    test("findById", async () => {
+        const res = await authModel.findById(1)
+        expect(res.username).toBe("varchar")
+    })
+
+    test("insert", async () => {
+        await authModel.insert({ username: "harry" })
+        const users = await db("users").select()
+        expect(users).toHaveLength(2)
+})
+
+    test("update", async () => {
+        await authModel.update(1, { username: "spetti" })
+        const users = await authModel.findById(1)
+        expect(users.username).toBe("spetti")
+    })
+
+    test("remove", async () => {
+        await authModel.remove(1)
+        const users = await authModel.list()
+        expect(users).toHaveLength(0)
+        console.log(users)
+    })
+})
