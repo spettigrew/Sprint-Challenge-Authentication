8b2e5117817bc817f821c0fd86a76c0192e05d60
spettigrew <sara.pettigrew5@gmail.com>
2020-01-24 18:29:08 -0700

    Authorized user setup in jokes-router file

--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', authenticate-middleware, authorizeUser("jokester"), (req, res) => {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
@@ -10,11 +10,24 @@ router.get('/', (req, res) => {
   axios
     .get('https://icanhazdadjoke.com/search', requestOptions)
     .then(response => {
-      res.status(200).json(response.data.results);
+      return res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      return res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 });
+
+function authorizeUser(role) {
+  return function (req, res, next) {
+    if (req.token && role === req.token.role) {
+      next()
+    } else {
+      console.log(req.token)
+      console.log(role)
+      return res.status(403).json({ message: "You are not authorized." })
+    }
+  }
+}
+
 
 module.exports = router;
