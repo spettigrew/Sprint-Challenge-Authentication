e6535c9a85a9e1f7e775e9f884b9e33049fb39ca
spettigrew <sara.pettigrew5@gmail.com>
2020-01-24 20:22:44 -0700

    Fixed import

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require('../middleware/authenticate-middleware.js/index.js.js');
+const authenticate = require('../middleware/authenticate-middleware')
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
