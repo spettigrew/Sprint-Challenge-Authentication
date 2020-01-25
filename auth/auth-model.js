ef0c0288f5ba0dddea9eccc1a5e00408e0ff1963
spettigrew <sara.pettigrew5@gmail.com>
2020-01-24 19:06:26 -0700

    First function setup

--- auth/auth-model.js
+++ auth/auth-model.js
@@ -1,11 +1,40 @@
-const bcrypt = require("bcryptjs")
+
 const db = require("../data/db-config")
 
-
+function list() {
+    return db("users")
+}
 
 module.exports = {
-    find,
-    findBy,
+    list,
     findById,
-    add,
+    insert,
+    update,
+    remove,
 }
+
+// function find() {
+//     return db("users")
+//         .select("id", "username")
+// }
+
+// function findBy(filter) {
+//     return db("users")
+//         .where(filter)
+//         .select("id", "username", "password", "department")
+// }
+
+// async function add(user) {
+//     user.password = await bcrypt.hash(user.password, 12)
+
+//     const [id] = await db("users")
+//         .insert(user)
+
+//     return findById(id)
+// }
+
+// function findById(id) {
+//     return db("users")
+//         .where({ id })
+//         .first("id", "username")
+// }
