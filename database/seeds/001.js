9012ced52d31c5c74a0d179371ddaee45834611a
spettigrew <sara.pettigrew5@gmail.com>
2020-01-27 14:50:01 -0700

    Seed data initiated and completed

---
+++ database/seeds/001.js
@@ -0,0 +1,11 @@
+
+exports.seed = async (knex) => {
+  
+  await knex("users").truncate()
+
+  await knex("users").insert([
+    { id: 1, username: 'spetti', password: "abc123" },
+    { id: 2, username: 'harry', password: "abc1234"},
+    { id: 3, username: 'spetti1', password: "abc12345"}
+      ]);
+};
