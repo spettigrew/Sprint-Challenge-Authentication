8d95142f1ddcd458ef450d85c99deabff12aaf31
spettigrew <sara.pettigrew5@gmail.com>
2020-01-27 20:31:28 -0700

    Tests setup for router

--- auth/authRouter.spec.js
+++ auth/authRouter.spec.js
@@ -17,21 +17,32 @@ test("welcome route", async () => {
     expect(res.body.message).toBe("Welcome to this API.")
  })
 
-test("get user list", async () => {
-    const res = await supertest(server).get("/api/users")
-    expect(res.status).toBe(200)
-    expect(res.type).toBe("application/json")
-    expect(res.body.length).toBeGreaterThan(0)
-    expect(res.body[0].id).toBe(1)
-    expect(res.body[0].username).toBe("spetti")
-})
+// test("get user list", async () => {
+//     const res = await supertest(server).get("/api/users")
+//     expect(res.status).toBe(200)
+//     expect(res.type).toBe("application/json")
+//     expect(res.body.length).toBeGreaterThan(1)
+//     expect(res.body[0].id).toBe(1)
+//     expect(res.body[0].username).toBe("spetti")
+// })
 
 test("create user route", async () => {
     const res = await supertest(server)
         .post("/api/users")
-        .send({ username: "spetti1" })
+        .insert({ username: "power" })
 
     expect(res.status).toBe(201)
     expect(res.type).toBe("application/json")
-    expect(res.body).toEqual({ id: 2, username: "spetti1" })
+    expect(res.body).toEqual({ id: 4, username: "power" })
+})
+
+test("check login status", async () => {
+    const res = await supertest(server)
+        .post("/api/users")
+        .send({ username: "spetti" })
+    
+        expect(res.status).toBe(200)
+        expect(res.type).toBe("application/json")
+        expect(res.body).toBe(username, password)
+
 })
