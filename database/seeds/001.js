6f5c9708c596ad155d26bd8ec57adf5a79b43c48
spettigrew <sara.pettigrew5@gmail.com>
2020-01-27 19:38:10 -0700

    Password changed in seeds file

--- database/seeds/001.js
+++ database/seeds/001.js
@@ -5,7 +5,7 @@ exports.seed = async (knex) => {
 
   await knex("users").insert([
     { id: 1, username: 'spetti', password: "abc123" },
-    { id: 2, username: 'harry', password: "abc1234"},
-    { id: 3, username: 'spetti1', password: "abc12345"}
+    { id: 2, username: 'harry', password: "abc123"},
+    { id: 3, username: 'spetti1', password: "abc123"}
       ]);
 };
